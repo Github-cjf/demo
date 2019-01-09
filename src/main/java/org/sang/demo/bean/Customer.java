5fe9cd52bc87a67924956ba7f5969f6986742653
Jian <chenjianfeng@ewell.cc>
2019-01-09 11:33:01 +0800

    test

--- src/main/java/org/sang/demo/bean/Customer.java
+++ src/main/java/org/sang/demo/bean/Customer.java
@@ -15,6 +15,7 @@ public class Customer {
     private Long id;
     private String firstName;
     private String lastName;
+
     public Customer() {}
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
